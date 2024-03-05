62ef9625504c6813322e9d09f7b175b8cd52afbc
Stepan Chernikov <schernikov@samokat.ru>
2024-03-05 17:31:11 +0300

    add install

--- src/settings/network.py
+++ src/settings/network.py
@@ -2,16 +2,16 @@ from pydantic import BaseModel
 
 
 class NetworkSettings(BaseModel):
-    STATUS_OK = 200
-    PORT = 8000
-    LOCAL_URL = f"http://127.0.0.1:{PORT}/"
-    LOCAL_SERVER_START = f"uvicorn src.server:app --port {PORT}"
-    KILL_LOCAL_SERVER = "kill -9 {pid}"
-    CONN_CHECK_TIMEOUT = 4
-    MAX_SERVER_AWAIT_SEC = 5
-    DEFAULT_LOCAL_NAME = "локальное"
-    NON_SERVER_CORE_URL = "<...>"
-    NON_SERVER_CORE_NAME = "встроенное"
+    STATUS_OK: int = 200
+    PORT: int = 8000
+    LOCAL_URL: str = f"http://127.0.0.1:{PORT}/"
+    LOCAL_SERVER_START: str = f"uvicorn src.server:app --port {PORT}"
+    KILL_LOCAL_SERVER: str = "kill -9 {pid}"
+    CONN_CHECK_TIMEOUT: int = 4
+    MAX_SERVER_AWAIT_SEC: int = 5
+    DEFAULT_LOCAL_NAME: str = "локальное"
+    NON_SERVER_CORE_URL: str = "<...>"
+    NON_SERVER_CORE_NAME: str = "встроенное"
 
 
 network_settings = NetworkSettings()
